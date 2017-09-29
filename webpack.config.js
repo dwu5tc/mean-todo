1f3d457e003c69b97aa666c395afe2ee566814ad
Dean <dean.wu55@gmail.com>
2017-09-29 00:00:02 -0400

    idk

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,48 @@
+// var webpack = require('webpack');
+// var path = require('path');
+
+// // module.exports???
+// // learn webpack
+// module.exports = {
+// 	devtool: 'inline-source-map',
+// 	entry: [
+// 		// what???
+// 		require.resolve('webpack-dev-server/client') + '?/',
+// 		require.resolve('webpack/hot/dev-server')
+// 		'/.src'
+// 	],
+// 	output: {
+// 		path: path.join(__dirname, 'public'),
+// 		filename: 'bundle.js'
+// 	},
+// 	resolve: {
+// 		modules: ['node_nodules', 'src'],
+// 		extensions: ['.js']
+// 	},
+// 	module: {
+// 		rules: [
+// 		{
+// 			test: /\.js$/,
+// 			exclude: /node_modules/,
+// 			loader: 'babel',
+// 			options: {
+// 				presets: ['es2015']
+// 			}
+// 		}
+// 		]
+// 	},
+// 	plugins: [
+// 		new webpack.HotModuleReplacementPlugin(),
+// 		new webpack.NoErrorsPlugin()
+// 	],
+// 	devServer: {
+// 		hot: true,
+// 		proxy: {
+// 			'*': 'http://localhost:3000'
+// 		}
+// 	}
+// }
+
 var webpack = require('webpack');
 var path = require('path');
 
@@ -6,8 +51,9 @@ var path = require('path');
 module.exports = {
 	devtool: 'inline-source-map',
 	entry: [
-		'webpack-dev-server/client?http://127.0.0.1:8080/',
-		'webpack/hot/only-dev-server',
+		// what???
+		require.resolve('webpack-dev-server/client') + '?/',
+		require.resolve('webpack/hot/dev-server')
 		'/.src'
 	],
 	output: {
